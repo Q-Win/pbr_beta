c5293f00b7b0375255cbbfa665e0646548ca3c4b
Quinn Krug <quinn.krug@gmail.com>
2019-04-26 14:45:05 -0600

    build form to add recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,24 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+
+    if recipe.save
+      redirect_to "/recipes"
+    else
+
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredients, :directions, :gif, :video, :link)
+  end
+
 end

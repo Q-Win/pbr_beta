78475f8f2f8d800eef9f047b436a950bfa7041a9
Quinn Krug <quinn.krug@gmail.com>
2019-04-26 17:31:46 -0600

    create recipe show page

---
+++ spec/features/recipes/show_page_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "recipe_index", type: :feature do
+
+  before(:each) do
+    @recipe_1 = Recipe.create(name: "cookies", ingredients: "flour, sugar", directions: "mix")
+    @recipe_2 = Recipe.create(name: "muffins", ingredients: "flour, sugar", directions: "mix")
+  end
+
+  it "user visits recipe show page" do
+
+    visit "/recipes/#{@recipe_2.id}"
+
+    expect(page).to have_content(@recipe_2.name)
+    expect(page).to have_content(@recipe_2.directions)
+    expect(page).to have_content("flour")
+    expect(page).to have_content("sugar")
+
+  end
+end

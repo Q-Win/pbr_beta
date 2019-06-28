36a4ea969a4919ca15ac0210d560c117395792ac
Quinn Krug <quinn.krug@gmail.com>
2019-06-28 16:26:05 -0600

    push updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_004807) do
+ActiveRecord::Schema.define(version: 2019_05_01_214434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2019_04_30_004807) do
     t.string "video"
     t.string "link"
     t.string "notes"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.integer "role", default: 0
   end
 
 end

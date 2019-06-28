adb8eed9fa032620ac543b56bef015388289ab99
Quinn Krug <quinn.krug@gmail.com>
2019-06-28 16:47:34 -0600

    set up route for new login

---
+++ spec/features/users/user_can_login_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "recipe_index", type: :feature do
+
+  before(:each) do
+    @recipe_1 = Recipe.create(name: "cookies", ingredients: "flour, sugar", directions: "mix")
+    @recipe_2 = Recipe.create(name: "muffins", ingredients: "flour, sugar", directions: "mix", notes: "is this here")
+    @user_1 = User.create(name: "Bob", password: "bobrulez")
+  end
+
+  it "user can login" do
+
+    visit "/login"
+
+    expect(page).to have_content("Username")
+    expect(page).to have_content("Password")
+  
+
+  end
+end

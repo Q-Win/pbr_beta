79e08753083b893d4f7ff9a63cc8b2ae7a0eee0c
Quinn Krug <quinn.krug@gmail.com>
2019-09-23 17:27:49 -0600

    create User show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-
+    if current_user.id != params[:id].to_i
+    redirect_to root_path
+    end
+    @user = current_user
   end
 
 end

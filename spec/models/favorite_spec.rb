8e503a96bbb3462e788dba13785602561cab6059
Quinn Krug <quinn.krug@gmail.com>
2019-09-25 18:30:05 -0600

    set up Favorite test

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Favorite, type: :model do
+
+  describe 'Relationships' do
+    it { should belong_to(:user) }
+    it { should belong_to(:recipe) }
+  end
+
+
+
 end

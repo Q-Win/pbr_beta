97fe528460b2f3c8dea1e77f494bdf804052d05b
Quinn Krug <quinn.krug@gmail.com>
2019-09-26 17:47:39 -0600

    fix User recipe relationship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     redirect_to root_path
     end
     @user = current_user
+    @favorites = @user.get_favorites
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,16 +4,6 @@ describe User, type: :model do
 
 
 
-  before(:each) do
-    @recipe_1 = Recipe.create(name: "cookies", ingredients: "flour, sugar", directions: "mix")
-    @recipe_2 = Recipe.create(name: "muffins", ingredients: "flour, sugar", directions: "mix", notes: "is this here")
-    @user_1 = User.create(name: "Bob", password: "bobrulez")
-    @favorite = Favorite.create(user_id: @user_1.id, recipe_id: @recipe_2.id)
-  end
 
-  it 'gets users favorites' do
-
-      expect(@user_1.favorites.first.recipe_id).to eq(@recipe_2.id)
-  end
 
 end

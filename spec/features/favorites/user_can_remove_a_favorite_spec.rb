6a664c0127fac61b40877f9e18025a253e929887
Quinn Krug <quinn.krug@gmail.com>
2019-10-14 16:29:24 -0600

    set up remove favorite test

---
+++ spec/features/favorites/user_can_remove_a_favorite_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe "recipe_index", type: :feature do
+
+  before(:each) do
+    @recipe_1 = Recipe.create(name: "cookies", ingredients: "flour, sugar", directions: "mix")
+    @recipe_2 = Recipe.create(name: "muffins", ingredients: "flour, sugar", directions: "mix", notes: "is this here")
+    @user_1 = User.create(name: "Bob", password: "bobrulez")
+    @favorite = Favorite.create(user_id: @user_1.id, recipe_id: @recipe_2.id)
+  end
+
+  it "user can remove a favorite" do
+    visit recipes_path
+    click_button 'Sign In'
+    expect(current_path).to eq(login_path)
+
+    fill_in :name, with: "Bob"
+    fill_in :password, with: "bobrulez"
+    click_button 'Login'
+    click_button 'Home'
+
+
+    expect(current_path).to eq(user_path(@user_1))
+    expect(page).to have_content("muffins")
+    expect(page).to have_content("Remove Favorite")
+  end
+
+
+
+end

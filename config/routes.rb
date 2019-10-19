dbbeb0a4cd629950779143bbbea5fb509b1b6742
Quinn Krug <quinn.krug@gmail.com>
2019-10-19 14:33:04 -0600

    add seed index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#index'
   get '/login', to: 'session#new'
+  get '/seed', to: 'seed#index'
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
   delete '/favorites', to: 'favorites#destroy'

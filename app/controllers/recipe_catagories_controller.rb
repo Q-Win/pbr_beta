5ac7089f4d51388730e929b83389dbea262b811e
Quinn Krug <quinn.krug@gmail.com>
2020-03-30 10:55:46 -0600

    added logic to reicpe_catagory controller create method to not make duplicate recipe catagory relationships

--- app/controllers/recipe_catagories_controller.rb
+++ app/controllers/recipe_catagories_controller.rb
@@ -7,8 +7,9 @@ class RecipeCatagoriesController < ApplicationController
   def create
     recipe_id = params[:recipe_id].to_i
     catagory_id = Catagory.find_by(name: params[:catagory_name]).id
-
-    binding.pry
+    if RecipeCatagory.where(recipe_id: recipe_id).find_by(catagory_id: catagory_id) == nil
+      RecipeCatagory.create(recipe_id: recipe_id, catagory_id: catagory_id)
+    end
   end
 
 end

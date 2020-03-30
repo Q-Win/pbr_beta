bf5b4f8cfa968a8d23c5a96b1dad8d7422d3ced7
Quinn Krug <quinn.krug@gmail.com>
2020-03-30 10:39:32 -0600

    recipe_catagory controller create method has both recipe and catagory ids

--- app/controllers/recipe_catagories_controller.rb
+++ app/controllers/recipe_catagories_controller.rb
@@ -6,6 +6,7 @@ class RecipeCatagoriesController < ApplicationController
 
   def create
     recipe_id = params[:recipe_id].to_i
+    catagory_id = Catagory.find_by(name: params[:catagory_name]).id
 
     binding.pry
   end
